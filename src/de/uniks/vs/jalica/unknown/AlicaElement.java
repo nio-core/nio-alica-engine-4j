27fc7a2e52940b65834cd936d1b990c1f6249401
Alex Jahl <ajahl.se@gmail.com>
2017-07-21 15:28:30 +0200

    add class

---
+++ src/de/uniks/vs/jalica/unknown/AlicaElement.java
@@ -0,0 +1,40 @@
+package de.uniks.vs.jalica.unknown;
+
+/**
+ * Created by alex on 17.07.17.
+ */
+public class AlicaElement {
+
+    protected long getId() {
+        return id;
+    }
+
+    protected void setId(long id) {
+        this.id = id;
+    }
+
+    private  long id;
+
+    protected String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    protected String getComment() {
+        return comment;
+    }
+
+    protected void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    protected  String comment;
+
+
+
+}
