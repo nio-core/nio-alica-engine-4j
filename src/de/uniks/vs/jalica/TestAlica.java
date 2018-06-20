1225a34157c6ffb8883b1bf4536297c59eecdb14
Alex Jahl <ajahl.se@gmail.com>
2018-06-20 11:11:01 +0200

    add first Test

--- src/de/uniks/vs/jalica/TestAlica.java
+++ src/de/uniks/vs/jalica/TestAlica.java
@@ -7,16 +7,23 @@ public class TestAlica {
     @Test
     public void testALICA() {
         Alica alicaAgent1 = new Alica("nio_zero");
-        Alica alicaAgent2 = new Alica("nio_one");
         alicaAgent1.start();
+
+        waitFor(2000);
+
+        Alica alicaAgent2 = new Alica("nio_one");
         alicaAgent2.start();
 
         while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            waitFor(100);
+        }
+    }
+
+    private void waitFor( long mseconds) {
+        try {
+            Thread.sleep(mseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
